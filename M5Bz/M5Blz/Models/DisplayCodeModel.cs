0833586cdefaaf5e95d7d97dc8cd5bd59448d2d1
Pgraveljs <93127781+Pgraveljs@users.noreply.github.com>
2022-02-03 06:50:16 -0500

    Clean auth

--- M5Bz/M5Blz/Models/DisplayCodeModel.cs
+++ M5Bz/M5Blz/Models/DisplayCodeModel.cs
@@ -4,9 +4,9 @@ namespace M5Blz.Models
 {
     public class DisplayCodeModel
     {
-        [Required]
-        [MinLength(4, ErrorMessage = "Le code doit avoir 4 caractère.")]
-        [MaxLength(4, ErrorMessage = "Le code doit avoir 4 caractère.")]
+        [Required(ErrorMessage = "Le code est requis.")]
+        [MinLength(4, ErrorMessage = "Le code doit avoir 4 caractères.")]
+        [MaxLength(4, ErrorMessage = "Le code doit avoir 4 caractères.")]
         public string Code { get; set; }
     }
 }
